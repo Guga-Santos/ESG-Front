7ffd763ae4f038c8f7c240fd335a31033a82c25f
GugaSantos <guga.santos.guga@gmail.com>
2023-09-14 08:31:14 -0300

    feat: example chart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import { ExampleChart } from "@/components/firstChart";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <ExampleChart />
     </main>
   )
 }
