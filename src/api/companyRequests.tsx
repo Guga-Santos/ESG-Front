7216f6ec91958cce0b3a425963718f1cbf69e023
GugaSantos <guga.santos.guga@gmail.com>
2023-09-14 22:45:02 -0300

    feat: companyRequests

---
+++ src/api/companyRequests.tsx
@@ -0,0 +1,39 @@
+import { client } from "@/api/client";
+import { ResponseAPI, companyBody } from "@/interfaces/Response.interface";
+
+
+export const createCompany = async (body: companyBody): Promise<ResponseAPI | any> =>  {
+  try {
+    const { data } = await client.post('/company', body);
+    return data;
+  } catch (err) {
+    console.error('Error: ', err);
+  }
+
+}
+
+export const getCompanies = async (): Promise<ResponseAPI[] | any> => {
+  try {
+    const { data } = await client.get('/companies');
+    return data;
+  } catch (err) {
+    console.error('Error: ', err);
+  }
+}
+
+export const deleteCompany = async (id: string) => {
+  try {
+    await client.delete(`/company/${id}`);
+  } catch (err) {
+    console.error('Error: ', err);
+  }
+}
+
+export const editCompany = async (id: string, editable: Partial<companyBody>): Promise<ResponseAPI | any> => {
+  try {
+    const { data } = await client.post(`/company/${id}`, editable);
+    return data;
+  } catch (err) {
+    console.error('Error: ', err);
+  }
+}
