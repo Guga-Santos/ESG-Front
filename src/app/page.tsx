2afc199418012c88735263175392b17e469094d2
GugaSantos <guga.santos.guga@gmail.com>
2023-09-14 23:25:25 -0300

    feat: CompanyProfiler

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,36 @@
+'use client'
+import { getCompanies } from "@/api/companyRequests";
 import Header from "@/components/Header";
 import CompanyProfiler from "@/components/companyProfiler";
+import { ResponseAPI } from "@/interfaces/Response.interface";
 import ChartPage from "@/pages/chartPages";
 import { PageContext } from "@/utils/PageContext";
+import { useEffect, useState } from "react";
 import data from '../mocks/fakeData.json';
 
-
 export default function Home() {
+  const [companies, setCompanies] = useState<ResponseAPI[] | []>([]);
+
+  useEffect(() => {
+    ;(async () => {
+      const data = await getCompanies();
+      setCompanies(data);
+    })();
+  }, []);
 
   return (
     <>
       <PageContext.Provider value={{}}>
-      <main className="flex min-h-screen flex-col items-center justify-between p-2">
-      <Header />
-      <CompanyProfiler />
-      <ChartPage {...data.company}/>
-      </main>
+        <main className="flex min-h-screen flex-col items-center justify-between p-2">
+          <Header />
+          { companies.map((company: ResponseAPI) => (
+            <CompanyProfiler 
+            logo={company.logo} 
+            id={company._id} 
+            key={company._id}/>
+          ))}
+          <ChartPage {...data.company} />
+        </main>
       </PageContext.Provider>
     </>
   )
--- src/components/companyProfiler.tsx
+++ src/components/companyProfiler.tsx
@@ -1,10 +1,23 @@
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
 
-export default function CompanyProfiler() {
+export default function CompanyProfiler(
+  { logo, id, key } 
+  : 
+  { logo: string, id: string, key: string }) {
   return (
-    <div className="border-1 w-3/4 h-96 bg-gradient-to-l from-indigo-950 rounded-2xl">
-      <figure className="m-8 border-2 border-white h-56 w-56">
-        <Image alt="Logo" src={""} />
+    <div className="
+    border-1 
+    w-2/3 
+    h-36 
+    mb-2
+    bg-gradient-to-l from-indigo-950 rounded-2xl">
+      <figure className="m-8 ml-20 h-20 w-20">
+        <img 
+        alt="Logo" 
+        src={logo} 
+        width={200} 
+        height={200}
+        className="rounded-lg"/>
       </figure>
     </div>
   )
