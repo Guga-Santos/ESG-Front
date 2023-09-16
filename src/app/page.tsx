9c042823cdb4a2570c1a53b8d39e112de6770809
GugaSantos <guga.santos.guga@gmail.com>
2023-09-15 23:21:37 -0300

    feat: search bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { getCompanies } from "@/api/companyRequests";
 import CompanyCard from "@/components/CompanyCard";
 import Header from "@/components/Header";
 import Loading from "@/components/Loading";
+import SearchInput from "@/components/SearchInput";
 import { ResponseAPI } from "@/interfaces/Response.interface";
 import { PageContext } from "@/utils/PageContext";
 import { useEffect, useState } from "react";
@@ -24,6 +25,7 @@ export default function Home() {
       <PageContext.Provider value={{ companies }}>
         <main className="flex min-h-screen flex-col items-center p-2">
           <Header />
+          <SearchInput />
           <div className="flex flex-wrap w-screen p-6 my-20 justify-center">
           { companies.length == 0 ? <Loading /> : companies.map((company: ResponseAPI) => (
             <CompanyCard
---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,31 @@
+export default function SearchInput() {
+  return (
+<form className="mt-28 w-80">   
+    <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only text-white">Search</label>
+    <div className="relative">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg className="w-4 h-4 text-gray-500 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+            </svg>
+        </div>
+        <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-200 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
+        <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 bg-blue-600 hover:bg-blue-700 dark:focus:ring-blue-800">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="3"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <circle cx="11" cy="11" r="8" />
+          <line x1="21" y1="21" x2="16.65" y2="16.65" />
+        </svg>
+        </button>
+    </div>
+</form>
+  )
+}
