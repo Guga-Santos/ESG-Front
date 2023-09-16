5f7c4c7317af80150404331beaa3e7fce8c49ba7
GugaSantos <guga.santos.guga@gmail.com>
2023-09-16 10:11:04 -0300

    feat: create stylessheet

---
+++ src/styles/styles.tsx
@@ -0,0 +1,9 @@
+export const styles = {
+  idPage: {
+    divContainer: 'h-full text-white',
+    divLoading: 'w-screen h-screen flex items-center justify-center',
+    divProfile: 'w-3/4 flex flex-wrap mx-auto mt-32 mb-8 justify-between',
+    image: 'rounded-md w-80 lg:w-40',
+    paragraph: 'w-80 lg:w-3/4 m-1 lg:m-3 text-justify'
+  }
+}
