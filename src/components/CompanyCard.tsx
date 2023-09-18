707917d3519010471a830c8021a370c025450d3f
GugaSantos <guga.santos.guga@gmail.com>
2023-09-17 23:15:29 -0300

    feat: companyCard styles sheet

--- src/components/CompanyCard.tsx
+++ src/components/CompanyCard.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 
+import { companyCardStyles } from "@/styles/CompanyCard.styles";
 import Link from "next/link";
 
 export default function CompanyCard(
@@ -8,41 +9,12 @@ export default function CompanyCard(
   { logo: string, id: string, key: string, name: string }) {
   return (
     <Link href={`company/${id}`}>
-    <div className="
-    border-1 
-    w-40 
-    h-40 
-    m-2
-    rounded-2xl
-    bg-[#FFFFFF]
-    dark:bg-slate-900
-    shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]
-    hover:shadow-[rgba(0,_0,_0,_0.4)_0px_3px_8px]
-    hover:cursor-pointer
-    active:brightness-150
-    flex
-    flex-col
-    justify-center
-    items-center
-    p-8">
+    <div className={companyCardStyles.firstDiv}>
       <div className="flex-col w-36 text-center">
       <figure className="h-24 w-24 mt-2 m-auto rounded-xl">
-        <img 
-        alt="Logo" 
-        src={logo} 
-        width={200} 
-        height={200}
-        className="rounded-lg select-none"/>
+        <img alt="Logo" src={logo} width={200} height={200} className="rounded-lg select-none"/>
       </figure>
-      <h1
-      className="
-      text-[#184E77]
-      dark:text-white
-      select-none
-      text-base
-      mt-2"
-      >{name}
-      </h1>
+      <h1 className={companyCardStyles.h1}>{name}</h1>
       </div>
     </div>
     </Link>
