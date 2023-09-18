9ee2fbcddc1bd04e899024f6bc7b547f7fcf4754
GugaSantos <guga.santos.guga@gmail.com>
2023-09-17 23:21:55 -0300

    feat: searchInput styles sheet

--- src/components/MixedChart.tsx
+++ src/components/MixedChart.tsx
@@ -42,29 +42,3 @@ export function MixedChart(props: IMixedProps){
     )
     
 }
-
-/**
-     <MixedChart  
-      id={"EEG"} 
-      categories={[1991, 1992, 1993, 1994, 1995, 1996, 1997]} 
-      name={"Natura-ESG"} 
-      series={[{
-        name: 'Carbono Neutro',
-        data: [44, 55, 41, 37, 22, 43, 21]
-      }, {
-        name: 'Descarte de resíduos',
-        data: [53, 32, 33, 52, 13, 43, 32]
-      }, {
-        name: 'Energia Limpa',
-        data: [12, 17, 11, 9, 15, 11, 20]
-      }, {
-        name: 'Produção sustentável',
-        data: [9, 7, 5, 8, 6, 9, 4]
-      }, {
-        name: 'Reciclagem',
-        data: [25, 12, 19, 32, 25, 24, 10]
-      }]}
-      type={"area"} 
-      height={500} 
-      width={800} />  
- */
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,4 +1,5 @@
 import { getCompanies } from "@/api/companyRequests";
+import { searchInputStyles } from "@/styles/SearchInput.styles";
 import { PageContext } from "@/utils/PageContext";
 import { useContext, useEffect } from "react";
 
@@ -23,33 +24,17 @@ export default function SearchInput() {
 
 
   return (
-<form className="
-  flex
-  items-center
-  justify-center
-  h-48 
-  pt-24
-  w-screen
-  bg-[#d9ed92]
-  dark:bg-[#00001e]"
-  >   
-    <div className="relative">
-        <div className="
-        absolute 
-        inset-y-0 
-        left-0 
-        flex 
-        items-center 
-        pl-3 
-        pointer-events-none">
-            <svg className="w-4 h-4 text-gray-500 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-            </svg>
+    <form className={searchInputStyles.form}>   
+      <div className="relative">
+        <div className={searchInputStyles.svgDiv}>
+          <svg className="w-4 h-4 text-gray-500 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+          </svg>
         </div>
         <input
         type="search" 
         id="default-search" 
-        className={darkmode ? "block w-full p-4 pl-10 text-sm text-gray-200 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" : "block w-full p-4 pl-10 text-sm text-black border border-gray-300 rounded-lg"} 
+        className={darkmode ? searchInputStyles.inputDark : searchInputStyles.inputLight } 
         placeholder="Procure uma empresa..." 
         onChange={(e) => handleChange(e.target.value)}
         required />
