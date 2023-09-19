347ba450a1835f71a3c97a1378bff2f36b583abe
GugaSantos <guga.santos.guga@gmail.com>
2023-09-19 00:19:06 -0300

    fix: waves responsivity

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -14,10 +14,12 @@ export default function Slider() {
   }, []);
 
   return (
-    <div className='flex flex-col min-h-screen relative bg-[#d9ed92]'>
+    <div className={isMobileDevice 
+    ? 'flex flex-col min-h-screen relative bg-[#d9ed92] -mb-56' 
+    : 'flex flex-col min-h-screen relative bg-[#d9ed92] -mb-12'}>
       <div className={isMobileDevice ? "w-screen pt-12 min-h-[80%]" : "w-screen"}>
           <img 
-          className={isMobileDevice ? "w-screen h-full object-cover" : "fixed w-screen h-full object-cover mt-12"}
+          className={isMobileDevice ? "fixed w-screen h-2/3 object-cover" : "fixed w-screen h-full object-cover mt-12"}
           src={isMobileDevice 
           ? "https://i.pinimg.com/564x/16/5a/cc/165acc364c6c56e52108cb7efe92ea70.jpg" 
           : "https://mercadoeconsumo.com.br/wp-content/uploads/2022/06/M3rc4doConsumo-shutterstock_2053227626.jpg"} alt="..." />
@@ -28,7 +30,7 @@ export default function Slider() {
       ? `${!darkmode ? 'bg-[#00001e]' : 'bg-[#d9ed92]'} waveOneMobil w-screen h-1/2 z-10 absolute top-80`
       : `${!darkmode ? 'waveOne bg-[#00001e]' : 'waveThree bg-[#d9ed92]'} w-screen h-1/2 z-10 absolute top-80 mt-4`} />
       <div className={isMobileDevice 
-        ? 'waveTwoMobil  w-screen bg-white h-1/2 z-10 absolute top-96 -mt-12' 
+        ? 'waveTwoMobil  w-screen bg-white h-2/3 z-10 absolute top-96 -mt-36' 
         : 'waveTwo bg-white w-screen h-1/2 z-10 absolute top-80 mt-4'} />
       <div className={isMobileDevice 
         ? `${darkmode ? 'bg-[#00001e]' : 'bg-[#d9ed92]'} waveThreeMobil w-screen h-2/3 z-10 absolute top-96 -mt-32` 
