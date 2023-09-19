13da4228fdb9c09e09fb1afb9b5722d04a136abb
GugaSantos <guga.santos.guga@gmail.com>
2023-09-19 17:15:19 -0300

    feat: routes phrases

--- src/pages/esg.tsx
+++ src/pages/esg.tsx
@@ -18,8 +18,9 @@ export default function Esg() {
     <div 
     className={`${darkmode ? 'dark': ''} w-screen min-h-screen bg-[#d9ed92] absolute flex flex-col`}>
       <Header />
-      <div className={`bg-white w-screen h-screen ${isMobileDevice ? 'mt-12' : 'mt-20'}`}>
+      <div className={`bg-white w-screen h-screen ${isMobileDevice ? 'mt-12' : 'mt-20'} relative`}>
         <div className="topWave bg-[#00001e] h-80" />
+        <h1 className={`text-[#00001e] ${isMobileDevice ? 'text-xl' : 'text-4xl'} absolute top-[15%] left-[10%]`}>VOCÊ SABE O QUE É ESG?</h1>
       </div>
     </div>
   )
