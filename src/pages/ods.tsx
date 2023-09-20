f3ddd5bd6b31a352254a439de41b0ecf2d43be30
GugaSantos <guga.santos.guga@gmail.com>
2023-09-20 13:25:02 -0300

    feat: modal base

--- src/pages/ods.tsx
+++ src/pages/ods.tsx
@@ -7,6 +7,7 @@ import ods from '../../public/images/ODS/odsPaths.json';
 export default function Ods() {
   const [darkmode, setDarkMode] = useState<boolean>();
   const [isMobileDevice, setIsMobileDevice] = useState<boolean>(false);
+  const [click, setClick] = useState<boolean>(false);
 
   useEffect(() => {
     ;(async () => {
@@ -31,12 +32,21 @@ export default function Ods() {
         key={index}
         width={160}
         height={160}
+        onClick={() => setClick(!click)}
         className="
         shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px] 
         hover:shadow-[rgba(50,50,93,0.5)_0px_6px_12px_-2px,_rgba(0,0,0,0.5)_0px_3px_7px_-3px] 
         active:shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px] cursor-pointer"/>
       ))}
       </div>
+      {
+        click 
+        && <div 
+        className="fixed z-50 bg-[rgba(0,0,0,0.5)] w-full p-4 h-[100%] w-[100%] flex justify-center items-center"
+        onClick={() => setClick(!click)}> 
+          <div className={`${isMobileDevice ? "h-[90%] w-[90%]" : "h-[80%] w-[80%]"} bg-white`}></div>
+        </div>
+      }
     </div>
   )
 }
